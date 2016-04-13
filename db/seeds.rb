f103513bafb36dd6a793f90648a775940dd7d572
Anna Natorilla <annagaiel@gmail.com>
2016-04-12 20:55:06 -0700

    Add the Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!([
+  {first_name: "Anna", last_name: "Nato", email: "anna@ym.com", phone_number: "123-456-7890"},
+  {first_name: "Jen", last_name: "Tom", email: "jen@ym.com", phone_number: "888-456-7890"},
+  {first_name: "Mark", last_name: "Uy", email: "mark@ym.com", phone_number: "500-456-7890"}
+])
+
+100.times do
+  User.create!(
+    {first_name: Faker::Name.name, last_name: Faker::Internet.user_name, email: Faker::Internet.email,
+      phone_number: Faker::PhoneNumber.cell_phone}
+      )
+end
